c2e2777a6895485764ee98144b9d834bf7c79ea5
wangdan1993 <1002275329@qq.com>
2018-09-26 09:37:30 +0800

    Initial Commit

---
+++ photoAndvideoUpload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  photoAndvideoUpload
+//
+//  Created by hisilc-mac002 on 2018/9/26.
+//  Copyright © 2018年 wangdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ photoAndvideoUpload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  photoAndvideoUpload
+//
+//  Created by hisilc-mac002 on 2018/9/26.
+//  Copyright © 2018年 wangdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
